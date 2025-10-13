5556a862cf6f51d1d5c781a60b4917b8bda02a4d
Mustafa Akgül <mustafaakguldev@icloud.com>
2025-10-13 23:04:07 +0300

    Add listing comment by approved status

--- app/comment/api/urls.py
+++ app/comment/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from app.comment.api.views import CommentAPIView
+from app.comment.api.views import CommentAPIView, ApprovedCommentsAPIView
 
 
 app_name = 'comment'
@@ -7,4 +7,5 @@ app_name = 'comment'
 
 urlpatterns = [
     path('', CommentAPIView.as_view(), name='tasks'),
+    path('status/approved/', ApprovedCommentsAPIView.as_view(), name='approved_comments'),
 ]
--- app/comment/api/views.py
+++ app/comment/api/views.py
@@ -46,3 +46,19 @@ class CommentAPIView(APIView):
             'payload': serializer.errors
         }
         return Response(data=resp, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ApprovedCommentsAPIView(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request, *args, **kwargs):
+        query = Comment.objects.filter(status='APPROVED')
+        serializer = CommentListSerializer(query, many=True)
+
+        resp = {
+            'status': 'true',
+            'message': 'successful',
+            'payload': serializer.data
+        }
+        response = Response(data=resp, status=status.HTTP_200_OK)
+        return response
